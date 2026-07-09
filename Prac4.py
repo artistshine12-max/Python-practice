4d9d203ee98bfaea0cd91e698ae2b01e03f62a28
Shine Chhaleriya <artistshine12@gmail.com>
2026-07-09 12:48:23 +0530

    Add input and sum functionality in Prac4.py
    Taking Input and then applying type-Conversion

---
+++ Prac4.py
@@ -0,0 +1,7 @@
+# Taking Input and applying the type-conversion
+=int(input("Enter number 1 "))
+b=int(input("Enter number 2 "))
+# Printing the numbers and their sum
+print("Number a is: ",a)
+print("Number b is: ",b)
+print("sum of a and b :", a+b)
