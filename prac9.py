12dc763c2bfdaa27bba7a183a598d2c961e4a3e7
Shine Chhaleriya <artistshine12@gmail.com>
2026-07-27 12:30:25 +0530

    Update prac9.py

--- prac9.py
+++ prac9.py
@@ -8,3 +8,16 @@ print(a)
 #Unlike strings, lists are mutable.
 #Lists are slice in the same ways as Strings:
 print(a[2:4])
+#Important List functions:
+a.append("HEllo")
+print(a)
+b= [2,1,3,6,7,10,9,5,4]
+b.sort()
+print(b)
+b.reverse()
+print(b)
+b.insert(3, "Shin Chan")
+print(b)
+b.pop(3)
+print(b)
+print(b.pop(3))
