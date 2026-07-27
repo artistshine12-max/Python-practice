42d141c3e0a6e428dde6e968c54b38521af7c96a
Shine Chhaleriya <artistshine12@gmail.com>
2026-07-27 14:43:25 +0530

    Add age validation with conditional statements

---
+++ prac12.py
@@ -0,0 +1,9 @@
+# Conditional statements in python:
+a= int(input("Enter your age: "))
+if(a>=18):
+    print("You are adult.")
+elif(a<0):
+    print("Invalid age.")
+
+else:
+    print("You are a minor.")
